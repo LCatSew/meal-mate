5de70498b0a236925f3a097434eb8d169af1cc77
Seth Schoenert <seth.schoenert@gmail.com>
2023-04-26 21:09:47 -0500

    added elements to sidebar and added correlated javascript for IDs. Button will need CSS styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,11 @@
 // CURRENTLY NOT DONE WITH THIS FUNCTION
 $(document).ready(function() {
     // This code handles the form submission for the sidebar input
-    $(/*html id here*/).submit(function(event) {
+    $('#recipeForm').submit(function(event) {
         event.preventDefault();
         
         // Gets the value of the input
-       var ingredient = $(/*html id here*/).val();
+       var ingredient = $('#ingredientInput').val();
 
        
        
