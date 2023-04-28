2e3025149f3cd5d39e9d5fe0265f69a604fa8560
gcot970 <127463356+gcot970@users.noreply.github.com>
2023-04-28 13:47:25 -0500

    Search History Somewhat Implemented
    Added saving history from search. Features to work on include duplicate check (and possibly a something that will correspond with the API to only save the searches that gather results). One of those features were started in this change.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,36 @@
 // main function that only runs when the DOM is ready 
 // CURRENTLY NOT DONE WITH THIS FUNCTION
 $(document).ready(function() {
-    // This code handles the form submission for the sidebar input
-    $('#recipeForm').submit(function(event) {
-        event.preventDefault();
-        
-        // Gets the value of the input
-       var ingredient = $('#ingredientInput').val();
+ 
+  var searchBox = $('#searchInput');
 
-       
-       
-    })
-});
+  $('#toggleSidebar').click(function() {
+      $('.ui.sidebar').sidebar('toggle');
+  })
+
+  // This code handles the form submission for the sidebar input
+  $('#recipeForm').submit(function(event) {
+      event.preventDefault();
+      var searchInput = searchBox.val();
+      // ajax call to request data from edamam
+     $.ajax('https://api.edamam.com/search', {
+      data: {
+      q: searchInput,
+      app_id: 'aa26a8c0', 
+      app_key: '4e5724065c8d4517c49380b4618935d5'	
+     },
+     success: function(data) {
+      var recipes = data.hits;
+
+      for (var i = 0; i < recipes.length; i++) {
+          var recipe = recipes[i].recipe;
+          console.log('Recipe:', recipe.label);
+      }
+  }
+     });
+     $('#searchInput').val();
+  });
+}); 
 
 
 // Gage's additions :-)
@@ -24,7 +43,8 @@ var searchBttn = document.querySelector('#searchButton');
 var searchBox = document.querySelector('#searchInput');
 var resultsBox = document.querySelector('#searchHistory');
 
-
+// Pushes history from input to array
+// Keeps array at 5 max
 searchBttn.addEventListener('click', function() {
   resultGet = searchBox.value;
   if (searchHistory.length < 5) {
@@ -46,10 +66,34 @@ $('.ui.sidebar')
 
 
 
-// Sidebar getHistory
+// Sidebar history
 function saveResults(){
+  //Resets the funny history area
+  resultsBox.innerHTML = "";
+
   // Save the string in LocalStorage
   localStorage.setItem('savedHistory', searchHistory);
 
   // I would really like this to update the DOM when ran :-)
+  for (i = 0; i < searchHistory.length; i++){
+    console.log(i);
+    var searchResult = document.createElement("p");
+    searchResult.textContent = searchHistory[i];
+    searchResult.classList.add("item");
+    resultsBox.appendChild(searchResult);
+  }
 };
+
+// History duplicate checker
+// Totally broken rn... please ignore :(
+function duplicateCheck(input){
+  for (i = 0; i < searchBox.History.length; i++){
+    if (input === searchHistory[i]){
+      searchHistory.splice(i,1)
+      searchHistory.unshift(input);
+    }
+    else{
+      return false;
+    }
+  }
+}
