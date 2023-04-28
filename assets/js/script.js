8e05b55e89821378ed5a13df687b94416f558649
gcot970 <127463356+gcot970@users.noreply.github.com>
2023-04-27 19:57:06 -0500

    Updated searchHistory, IDs on index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,44 @@ $(document).ready(function() {
        
     })
 });
+
+
+// Gage's additions :-)
+
+// An array keeping the history
+var searchHistory = [];
+
+// Search button
+var searchBttn = document.querySelector('#searchButton');
+var searchBox = document.querySelector('#searchInput');
+var resultsBox = document.querySelector('#searchHistory');
+
+
+searchBttn.addEventListener('click', function() {
+  resultGet = searchBox.value;
+  if (searchHistory.length < 5) {
+    searchHistory.push(resultGet);
+  } else {
+    searchHistory.shift();
+    searchHistory.push(resultGet);
+  }
+
+  saveResults();
+});
+
+
+// Sidebar toggle
+// https://semantic-ui.com/modules/sidebar.html
+$('.ui.sidebar')
+  .sidebar('toggle')
+;
+
+
+
+// Sidebar getHistory
+function saveResults(){
+  // Save the string in LocalStorage
+  localStorage.setItem('savedHistory', searchHistory);
+
+  // I would really like this to update the DOM when ran :-)
+};
