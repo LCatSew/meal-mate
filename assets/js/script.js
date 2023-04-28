d5dbaeb5294747552a2d8fbf3edbf9568b7f959e
LCatSew <119518206+LCatSew@users.noreply.github.com>
2023-04-28 13:49:39 -0500

    recloned git repository to local computer, had to go through and add missing peices from what was on git. please review general code carefully so we make sure nothing is being taken out or is missing. also added modal to HTML and behavior to script file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,75 @@
 // main function that only runs when the DOM is ready 
 // CURRENTLY NOT DONE WITH THIS FUNCTION
 $(document).ready(function() {
+ 
+    var searchBox = $('#searchInput');
+ 
+    $('#toggleSidebar').click(function() {
+        $('.ui.sidebar').sidebar('toggle');
+    })
+
     // This code handles the form submission for the sidebar input
     $('#recipeForm').submit(function(event) {
         event.preventDefault();
-        
-        // Gets the value of the input
-       var ingredient = $('#ingredientInput').val();
+        var searchInput = searchBox.val();
+        // ajax call to request data from edamam
+       $.ajax('https://api.edamam.com/search', {
+        data: {
+        q: searchInput,
+        app_id: 'aa26a8c0', 
+        app_key: '4e5724065c8d4517c49380b4618935d5'	
+       },
+       success: function(data) {
+        var recipes = data.hits;
 
+        for (var i = 0; i < recipes.length; i++) {
+            var recipe = recipes[i].recipe;
+            console.log('Recipe:', recipe.label);
+        }
+    }
+       });
+       $('#searchInput').val();
+    });
+}); 
        
-       
-    })
-});
+// An array keeping the history
+var searchHistory = [];
+
+// Search button
+var searchBttn = $('#searchButton');
+var resultsBox = $('#searchHistory');
+
+$('#searchButton').click(function() {
+    var resultGet = $('#searchInput').val();
+
+    if (searchHistory.length < 5) {
+      searchHistory.push(resultGet);
+    } else {
+      searchHistory.shift();
+      searchHistory.push(resultGet);
+    }
+
+    saveResults();
+  });
+
+
+
+// Sidebar getHistory
+function saveResults(){
+  // Save the string in LocalStorage
+  localStorage.setItem('savedHistory', JSON.stringify(searchHistory));
+  
+  $("#searchHistory").empty();
+
+  searchHistory.forEach(function (item) {
+    var historyBtn = $("<button>")
+      .addClass("ui button search-history-btn")
+      .text(item)
+      .attr("data-search", item);
+    $("#searchHistory").append(historyBtn);
+  });
+};
+
+$('.longer.modal')
+    .modal('show')
+;
