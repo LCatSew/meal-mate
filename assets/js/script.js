8d2713a4f947b83b87ef0ad79949e10e5f1fa5fd
gcot970 <127463356+gcot970@users.noreply.github.com>
2023-05-01 11:42:13 -0500

    JavaScript Function Additions + HTML Cleanup
    Within this change, I've cleaned up the HTML (included a reset stylesheet), and wrote new functions for a temporary test/debug menu, a new card function, and a duplicate checker (that I hope to incorporate into the process of saving the history). I've added three more comments about functions that I hope will be added. Those being an API parser for TCDB, a pairing function, and a modal/details function. I hope to refine all of these soon.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,47 @@
 // main function that only runs when the DOM is ready 
 // CURRENTLY NOT DONE WITH THIS FUNCTION
-$(document).ready(function() {
- 
+$(document).ready(function () {
+
   var searchBox = $('#searchInput');
 
-  $('#toggleSidebar').click(function() {
-      $('.ui.sidebar').sidebar('toggle');
+  $('#toggleSidebar').click(function () {
+    $('.ui.sidebar').sidebar('toggle');
   })
 
   // This code handles the form submission for the sidebar input
-  $('#recipeForm').submit(function(event) {
-      event.preventDefault();
-      var searchInput = searchBox.val();
-      // ajax call to request data from edamam
-     $.ajax('https://api.edamam.com/search', {
+  $('#recipeForm').submit(function (event) {
+    event.preventDefault();
+    var searchInput = searchBox.val();
+    // ajax call to request data from edamam
+    $.ajax('https://api.edamam.com/search', {
       data: {
-      q: searchInput,
-      app_id: 'aa26a8c0', 
-      app_key: '4e5724065c8d4517c49380b4618935d5'	
-     },
-     success: function(data) {
-      var recipes = data.hits;
+        q: searchInput,
+        app_id: 'aa26a8c0',
+        app_key: '4e5724065c8d4517c49380b4618935d5'
+      },
+      success: function (data) {
+        var recipes = data.hits;
 
-      for (var i = 0; i < recipes.length; i++) {
+        for (var i = 0; i < recipes.length; i++) {
           var recipe = recipes[i].recipe;
           console.log('Recipe:', recipe.label);
+        }
       }
-  }
-     });
-     $('#searchInput').val();
+    });
+    $('#searchInput').val();
   });
-}); 
-
-
-// Gage's additions :-)
+});
 
 // An array keeping the history
 var searchHistory = [];
 
 // Search button
-var searchBttn = document.querySelector('#searchButton');
-var searchBox = document.querySelector('#searchInput');
-var resultsBox = document.querySelector('#searchHistory');
+var searchBttn = $('#searchButton');
+var resultsBox = $('#searchHistory');
 
-// Pushes history from input to array
-// Keeps array at 5 max
-searchBttn.addEventListener('click', function() {
-  resultGet = searchBox.value;
+$('#searchButton').click(function () {
+  var resultGet = $('#searchInput').val();
+
   if (searchHistory.length < 5) {
     searchHistory.push(resultGet);
   } else {
@@ -58,42 +53,152 @@ searchBttn.addEventListener('click', function() {
 });
 
 
-// Sidebar toggle
-// https://semantic-ui.com/modules/sidebar.html
-$('.ui.sidebar')
-  .sidebar('toggle')
-;
 
-
-
-// Sidebar history
-function saveResults(){
-  //Resets the funny history area
-  resultsBox.innerHTML = "";
-
+// Sidebar getHistory
+function saveResults() {
   // Save the string in LocalStorage
-  localStorage.setItem('savedHistory', searchHistory);
+  localStorage.setItem('savedHistory', JSON.stringify(searchHistory));
 
-  // I would really like this to update the DOM when ran :-)
-  for (i = 0; i < searchHistory.length; i++){
-    console.log(i);
-    var searchResult = document.createElement("p");
-    searchResult.textContent = searchHistory[i];
-    searchResult.classList.add("item");
-    resultsBox.appendChild(searchResult);
-  }
+  $("#searchHistory").empty();
+
+  searchHistory.forEach(function (item) {
+    var historyBtn = $("<button>")
+      .addClass("ui button search-history-btn")
+      .text(item)
+      .attr("data-search", item);
+    $("#searchHistory").append(historyBtn);
+  });
 };
 
-// History duplicate checker
-// Totally broken rn... please ignore :(
-function duplicateCheck(input){
-  for (i = 0; i < searchBox.History.length; i++){
-    if (input === searchHistory[i]){
-      searchHistory.splice(i,1)
+
+// ====================================
+//      Gage's JavaScript portion
+// ====================================
+//         TheCocktailDB API
+// https://www.thecocktaildb.com/api.php
+// ------------------------------------
+
+// ------------------------------------
+// Search
+// ------------------------------------
+var testMenuDiv = document.getElementById("testMenu");
+
+
+// ------------------------------------
+//             Test menu
+// ------------------------------------
+// Enable the test menu:
+testNav = document.getElementById('testMenu');
+
+$('#searchButton').click(function () {
+  var resultGet = $('#searchInput').val();
+
+  if (resultGet === "test") {
+    console.log("Test menu has been enabled!");
+    var menuHTML = `
+     <p>Test menu:</p>
+      <a id="testCard" href="#">Create a card</a>
+      <a id="testSearch" href="#">Search Results</a>
+      <a id="testHistory" href="#">Add to history</a>
+      <a id="testClearHis" href="#">Clear history</a>
+      <a id="testDetails" href="#">Modal Toggle</a>`;
+
+    var testMenuDiv = document.getElementById("testMenu");
+    testMenuDiv.innerHTML = menuHTML;
+
+    testMenuDiv.style.backgroundColor = 'black';
+    testMenuDiv.style.color = 'white';
+
+    // Menu Options:
+    var testCardEl = document.getElementById('testCard');
+    $(testCardEl).click(function () {
+      console.log("Test card clicked!");
+
+      newCard("Meatloaf", "./assets/img/dummy.png", "Grape juice", "./assets/img/dummy.png")
+    });
+
+
+    var clearHistoryEl = document.getElementById('testClearHis');
+    $(clearHistoryEl).click(function () {
+      console.log("History cleared!");
+      searchHistory = [];
+      console.log("The array is now:" + searchHistory);
+      saveResults();
+    });
+  }
+});
+
+
+var testSearchEl = document.getElementById('testSearch');
+var testHisEl = document.getElementById('testHistory');
+var testCHEl = document.getElementById('testClearHis');
+var testDEl = document.getElementById('testDetails');
+
+
+// ------------------------------------
+//        Create-a-card Function
+// ------------------------------------
+// Creates a new card based on the
+// arguments given to the function.
+// ------------------------------------
+function newCard(cusineTitle, cuisineImage, wineTitle, wineImage) {
+  var cardHTML = `
+  <div class="card">
+    <div class="ui divided equal width grid">
+      <div class="column">
+        <img src="${cuisineImage}" class="ui fluid image mini">
+      </div>
+      <div class="column">
+        <img src="${wineImage}" class="ui fluid image mini">
+      </div>
+    </div>
+    <div class="content">
+      <div class="header">${wineTitle}</div>
+      <div class="meta">
+        <a>goes well with ${cusineTitle}</a>
+      </div>
+  </div>`;
+
+  $('#searchResults').append(cardHTML);
+};
+// ------------------------------------
+//          Pairing Function
+// ------------------------------------
+// Creates a pairing based on the
+// arguments given to the function.
+// ------------------------------------
+function pairCocktail(cuisine) {
+  // Region
+
+  // Similar flavor
+
+  // Similar ingredient
+
+  // Contrast pairing
+
+};
+// ------------------------------------
+//          Duplicate Check
+// ------------------------------------
+// Search function that returns true
+// or false if there is a duplicate in
+// the array searchHistory.
+// ------------------------------------
+function duplicateCheck(input) {
+  for (i = 0; i < searchBox.History.length; i++) {
+    if (input === searchHistory[i]) {
+      searchHistory.splice(i, 1)
       searchHistory.unshift(input);
     }
-    else{
+    else {
       return false;
     }
   }
-}
+};
+// ------------------------------------
+//          Details Modal
+// ------------------------------------
+// Returns details of the recipe and
+// pairing. Details will come from
+// pairCocktail().
+// ------------------------------------
