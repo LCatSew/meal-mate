8a132c8c8c98e04751a45ff9f3259db937d798e3
gcot970 <127463356+gcot970@users.noreply.github.com>
2023-05-04 18:16:20 -0500

    Updated styles and script for search icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,17 @@
-// main function that only runs when the DOM is ready
+// main function that only runs when the DOM is ready 
 // CURRENTLY NOT DONE WITH THIS FUNCTION
 $(document).ready(function () {
-  var searchBox = $("#searchInput");
 
-  $("#startButton").click(function () {
-    $(".ui.sidebar").sidebar("toggle");
-  });
+  var searchBox = $('#searchInput');
 
-  // Hide landing page and show the rest of the content
-  $("#searchButton").click(function () {
-    $(".landing-page").hide();
-    $("#toggleSidebar").show();
-    $(".pusher").show();
-  });
-
-
-  // Hide landing page and show the rest of the content
-  $("#searchButton").click(function () {
-    $(".landing-page").hide();
-    $("#toggleSidebar").show();
-    $(".pusher").show();
-
-  });
-
-
-  // Modal button
-  $('#show-modal-btn').click(function () {
-    $('#my-modal').modal('show');
-  });
-
+  $('#toggleSidebar').click(function () {
+    $('.ui.sidebar').sidebar('toggle');
+  })
 
   // This code handles the form submission for the sidebar input
-  $("#recipeForm").submit(function (event) {
+  $('#recipeForm').submit(function (event) {
     event.preventDefault();
     var searchInput = searchBox.val();
-
-    var result = {}; // Important!! Empty object to put cocktail in. Best to rename it later.
-    console.log("Test search clicked!");
-
-    // Temp serach term solely for test button
     // ajax call to request data from edamam
     $.ajax('https://api.edamam.com/search', {
       data: {
@@ -47,178 +20,29 @@ $(document).ready(function () {
         app_key: '4e5724065c8d4517c49380b4618935d5'
       },
       success: function (data) {
-        $("#searchResults").html("");
         var recipes = data.hits;
 
-        // Iterates through recipes to put each recipe into pairCocktail
-        // to get search query for TCDB.
         for (var i = 0; i < recipes.length; i++) {
           var recipe = recipes[i].recipe;
-          dataCocktail = pairCocktail(recipe);
-
-          // If statement to see if it needs to perform search or to get random.
-          if (dataCocktail != "random") {
-            $.ajax({
-              url: 'https://www.thecocktaildb.com/api/json/v1/1/search.php',
-              data: { s: dataCocktail },
-              success: function (data) {
-                var drinks = data.drinks;
-                var randomDrink = Math.floor(Math.random() * drinks.length);
-                result.title = drinks[randomDrink].strDrink;
-                result.image = drinks[randomDrink].strDrinkThumb;
-
-                for (var x = 0; x < recipes.length; x++) { // Had to do this since ajax is async, Might come up with better later
-                  recipe = recipes[x].recipe;
-                  console.log("Recipe label:");
-                  var recipeTitle = recipe.label;
-                  console.log(recipeTitle);
-                  console.log("Recipe image:");
-                  var recipeImage = recipe.image;
-                  console.log(recipeImage);
-                  dataCocktail = pairCocktail(recipe);
-
-                  newCard(recipeTitle, recipeImage, result.title, result.image);
-
-                }
-              }
-            });
-          } else {
-            $.ajax({
-              url: 'https://www.thecocktaildb.com/api/json/v1/1/random.php',
-              dataType: 'json',
-              success: function (data) {
-                var drinks = data.drinks;
-                var randomDrink = Math.floor(Math.random() * drinks.length);
-                var finalDrink = drinks[randomDrink].strDrink;
-
-                result.title = drinks[randomDrink].strDrink;
-                result.image = drinks[randomDrink].strDrinkThumb;
-
-                for (var x = 0; x < recipes.length; x++) { // Refer to line 170
-                  recipe = recipes[x].recipe;
-                  console.log("Recipe label:");
-                  var recipeTitle = recipe.label;
-                  console.log(recipeTitle);
-                  console.log("Recipe image:");
-                  var recipeImage = recipe.image;
-                  console.log(recipeImage);
-                  dataCocktail = pairCocktail(recipe);
-
-                  newCard(recipeTitle, recipeImage, result.title, result.image);
-
-                }
-              }
-            });
-          }
+          console.log('Recipe:', recipe.label);
+          console.log(recipe);
         }
       }
     });
+    $('#searchInput').val();
 
-    $("#searchInput").val();
   });
-
-
-
-  // This code handles the history button
-  $('#searchHistory').on('click', '.search-history-btn', function () {
-    var searchInput = $(this).text();
-
-
-    var result = {}; // Important!! Empty object to put cocktail in. Best to rename it later.
-
-    // Temp serach term solely for test button
-    // ajax call to request data from edamam
-    $.ajax('https://api.edamam.com/search', {
-      data: {
-        q: searchInput,
-        app_id: 'aa26a8c0',
-        app_key: '4e5724065c8d4517c49380b4618935d5'
-      },
-      success: function (data) {
-        $("#searchResults").html("");
-        var recipes = data.hits;
-
-        // Iterates through recipes to put each recipe into pairCocktail
-        // to get search query for TCDB.
-        for (var i = 0; i < recipes.length; i++) {
-          var recipe = recipes[i].recipe;
-          dataCocktail = pairCocktail(recipe);
-
-          // If statement to see if it needs to perform search or to get random.
-          if (dataCocktail != "random") {
-            $.ajax({
-              url: 'https://www.thecocktaildb.com/api/json/v1/1/search.php',
-              data: { s: dataCocktail },
-              success: function (data) {
-                var drinks = data.drinks;
-                var randomDrink = Math.floor(Math.random() * drinks.length);
-                result.title = drinks[randomDrink].strDrink;
-                result.image = drinks[randomDrink].strDrinkThumb;
-
-                for (var x = 0; x < recipes.length; x++) { // Had to do this since ajax is async, Might come up with better later
-                  recipe = recipes[x].recipe;
-                  console.log("Recipe label:");
-                  var recipeTitle = recipe.label;
-                  console.log(recipeTitle);
-                  console.log("Recipe image:");
-                  var recipeImage = recipe.image;
-                  console.log(recipeImage);
-                  dataCocktail = pairCocktail(recipe);
-
-                  newCard(recipeTitle, recipeImage, result.title, result.image);
-
-                }
-              }
-            });
-          } else {
-            $.ajax({
-              url: 'https://www.thecocktaildb.com/api/json/v1/1/random.php',
-              dataType: 'json',
-              success: function (data) {
-                var drinks = data.drinks;
-                var randomDrink = Math.floor(Math.random() * drinks.length);
-                var finalDrink = drinks[randomDrink].strDrink;
-
-                result.title = drinks[randomDrink].strDrink;
-                result.image = drinks[randomDrink].strDrinkThumb;
-
-                for (var x = 0; x < recipes.length; x++) { // Refer to line 170
-                  recipe = recipes[x].recipe;
-                  console.log("Recipe label:");
-                  var recipeTitle = recipe.label;
-                  console.log(recipeTitle);
-                  console.log("Recipe image:");
-                  var recipeImage = recipe.image;
-                  console.log(recipeImage);
-                  dataCocktail = pairCocktail(recipe);
-
-                  newCard(recipeTitle, recipeImage, result.title, result.image);
-
-                }
-              }
-            });
-          }
-        }
-      }
-    });
-  });
-
 });
-
-// Responses without this status will trigger error conditions
-$.fn.api.settings.successTest = function (response) {
-  return response.status == "OK";
-};
 
 // An array keeping the history
 var searchHistory = [];
 
 // Search button
-var searchBttn = $("#searchButton");
-var resultsBox = $("#searchHistory");
+var searchBttn = $('#searchButton');
+var resultsBox = $('#searchHistory');
 
-$("#searchButton").click(function () {
-  var resultGet = $("#searchInput").val();
+$('#searchButton').click(function () {
+  var resultGet = $('#searchInput').val();
 
   if (searchHistory.length < 5) {
     searchHistory.push(resultGet);
@@ -230,6 +54,8 @@ $("#searchButton").click(function () {
   saveResults();
 });
 
+
+
 // Sidebar getHistory
 function saveResults() {
   // Save the string in LocalStorage
@@ -239,12 +65,176 @@ function saveResults() {
 
   searchHistory.forEach(function (item) {
     var historyBtn = $("<button>")
-      .addClass("search-history-btn")
+      .addClass("ui button search-history-btn")
       .text(item)
       .attr("data-search", item);
     $("#searchHistory").append(historyBtn);
   });
 };
+
+
+// ====================================
+//      Gage's JavaScript portion
+// ====================================
+// ------------------------------------
+// Search
+// ------------------------------------
+var testMenuDiv = document.getElementById("testMenu");
+
+
+// ------------------------------------
+//             Test menu
+// ------------------------------------
+// Enable the test menu:
+testNav = document.getElementById('testMenu');
+
+$('#searchButton').click(function () {
+  var resultGet = $('#searchInput').val();
+
+  if (resultGet === "test") {
+    console.log("Test menu has been enabled!");
+    var menuHTML = `
+     <p>Test menu:</p>
+      <a id="testCard" href="#">Create a card</a>
+      <a id="testSearch" href="#">Search Results</a>
+      <a id="testHistory" href="#">Add to history</a>
+      <a id="testClearHis" href="#">Clear history</a>
+      <a id="testDetails" href="#">Modal Toggle</a>`;
+
+    var testMenuDiv = document.getElementById("testMenu");
+    testMenuDiv.innerHTML = menuHTML;
+
+    testMenuDiv.style.backgroundColor = 'black';
+    testMenuDiv.style.color = 'white';
+
+    // Menu Options:
+    var testCardEl = document.getElementById('testCard');
+    $(testCardEl).click(function () {
+      console.log("Test card clicked!");
+      newCard("Meatloaf", "./assets/img/dummy.png", "Grape juice", "./assets/img/dummy.png")
+    });
+
+    var searchCardEl = document.getElementById('testSearch');
+    $(searchCardEl).click(function () {
+
+
+
+      var result = {};
+
+
+
+      console.log("Test card clicked!");
+
+      var searchInput = "pizza";
+      // ajax call to request data from edamam
+      $.ajax('https://api.edamam.com/search', {
+        data: {
+          q: searchInput,
+          app_id: 'aa26a8c0',
+          app_key: '4e5724065c8d4517c49380b4618935d5'
+        },
+        success: function (data) {
+          var recipes = data.hits;
+
+          for (var i = 0; i < recipes.length; i++) {
+            var recipe = recipes[i].recipe;
+            dataCocktail = pairCocktail(recipe);
+
+
+            if (dataCocktail != "random") {
+              $.ajax({
+                url: 'https://www.thecocktaildb.com/api/json/v1/1/search.php',
+                data: { s: dataCocktail },
+                success: function (data) {
+                  console.log("In the if statement")
+                  console.log(recipeTitle)
+                  console.log(i);
+
+                  var drinks = data.drinks;
+                  console.log(drinks);
+                  var randomDrink = Math.floor(Math.random() * drinks.length);
+                  result.title = drinks[randomDrink].strDrink;
+                  result.image = drinks[randomDrink].strDrinkThumb;
+
+                  for (var x = 0; x < recipes.length; x++) {
+                    recipe = recipes[x].recipe;
+                    console.log("Recipe label:");
+                    var recipeTitle = recipe.label;
+                    console.log(recipeTitle);
+                    console.log("Recipe image:");
+                    var recipeImage = recipe.image;
+                    console.log(recipeImage);
+                    dataCocktail = pairCocktail(recipe);
+
+                    newCard(recipeTitle, recipeImage, result.title, result.image);
+
+                  }
+
+                }
+              });
+            } else {
+              $.ajax({
+                url: 'https://www.thecocktaildb.com/api/json/v1/1/random.php',
+                dataType: 'json',
+                success: function (data) {
+                  console.log("In the if statement");
+                  console.log(recipeTitle);
+                  console.log(i);
+
+
+                  var drinks = data.drinks;
+                  var randomDrink = Math.floor(Math.random() * drinks.length);
+                  var finalDrink = drinks[randomDrink].strDrink;
+
+                  result.title = drinks[randomDrink].strDrink;
+                  result.image = drinks[randomDrink].strDrinkThumb;
+
+                  for (var x = 0; x < recipes.length; x++) {
+                    recipe = recipes[x].recipe;
+                    console.log("Recipe label:");
+                    var recipeTitle = recipe.label;
+                    console.log(recipeTitle);
+                    console.log("Recipe image:");
+                    var recipeImage = recipe.image;
+                    console.log(recipeImage);
+                    dataCocktail = pairCocktail(recipe);
+
+                    newCard(recipeTitle, recipeImage, result.title, result.image);
+
+                  }
+                }
+              });
+            }
+            
+          }
+
+        }
+      });
+
+
+
+
+
+    });
+
+
+
+
+    var clearHistoryEl = document.getElementById('testClearHis');
+    $(clearHistoryEl).click(function () {
+      console.log("History cleared!");
+      searchHistory = [];
+      console.log("The array is now:" + searchHistory);
+      saveResults();
+    });
+  }
+});
+
+
+var testSearchEl = document.getElementById('testSearch');
+var testHisEl = document.getElementById('testHistory');
+var testCHEl = document.getElementById('testClearHis');
+var testDEl = document.getElementById('testDetails');
 
 
 // ------------------------------------
@@ -255,27 +245,24 @@ function saveResults() {
 // ------------------------------------
 function newCard(cusineTitle, cuisineImage, wineTitle, wineImage) {
   var cardHTML = `
-    <div class="card">
-      <div class="ui divided equal width centered grid" id="card-grid">
-        <div class="column">
-          <img src="${wineImage}" class="ui fluid medium image ">
-        </div>
-        <div class="column">
-          <img src="${cuisineImage}" class="ui fluid medium image ">
-        </div>
+  <div class="card">
+    <div class="ui divided equal width grid">
+      <div class="column">
+        <img src="${cuisineImage}" class="ui fluid image mini">
       </div>
-      <div class="content">
-        <div class="header">${wineTitle}</div>
-        <div class="meta">
-          <a>goes well with ${cusineTitle}</a>
-        </div>
+      <div class="column">
+        <img src="${wineImage}" class="ui fluid image mini">
       </div>
-    </div>`;
+    </div>
+    <div class="content">
+      <div class="header">${wineTitle}</div>
+      <div class="meta">
+        <a>goes well with ${cusineTitle}</a>
+      </div>
+  </div>`;
 
   $('#searchResults').append(cardHTML);
 };
-
-
 // ------------------------------------
 //          Pairing Function
 // ------------------------------------
@@ -285,14 +272,13 @@ function newCard(cusineTitle, cuisineImage, wineTitle, wineImage) {
 //         TheCocktailDB API
 // https://www.thecocktaildb.com/api.php
 // ------------------------------------
-function pairCocktail(cuisineData) {
-  // function is expecting entire recipe object
+// var resultCocktail = {};
 
+
+function pairCocktail(cuisineData) {
   var chosenDrink = "";
 
-  // Pairs with dishtype.
-  // If dishtype is not present, it will return "random".
-  // Will always return a string for TCDB search query.
+  console.log(cuisineData.dishType);
   if (cuisineData.dishType[0] == "bread" || cuisineData.dishType[0] == "pizza" || cuisineData.dishType[0] == "sandwiches") {
     chosenDrink = "beer";
   } else if (cuisineData.dishType[0] == "alcohol cocktail" || cuisineData.dishType[0] == "drinks") {
@@ -327,9 +313,78 @@ function pairCocktail(cuisineData) {
     chosenDrink = "random";
   };
 
+
+  //if (chosenDrink != "random") {
+  // getCocktail(chosenDrink);
+
+  //} else {
+  // getRandom();
+
+  //}
+
   return chosenDrink;
 };
 
+
+// Please remove in final ---- move to document ready function
+/* 
+async function getCocktail(chosenDrink) {
+  var data = await $.ajax({
+    url: 'https://www.thecocktaildb.com/api/json/v1/1/search.php',
+    data: { s: chosenDrink }
+  });
+
+  var drinks = data.drinks;
+  var randomDrink = Math.floor(Math.random() * drinks.length);
+  resultCocktail.title = drinks[randomDrink].strDrink;
+  resultCocktail.image = drinks[randomDrink].strDrinkThumb;
+
+  cocktailTitle = resultCocktail.title;
+  cocktailImg = resultCocktail.image;
+
+  return resultCocktail;
+}
+*/
+
+// Please remove in final ---- move to document ready function
+/* 
+async function getRandom() {
+  var response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+  var data = await response.json();
+
+  var drinks = data.drinks;
+  var randomDrink = Math.floor(Math.random() * drinks.length);
+  var finalDrink = drinks[randomDrink].strDrink;
+
+  resultCocktail.title = drinks[randomDrink].strDrink;
+  resultCocktail.image = drinks[randomDrink].strDrinkThumb;
+
+  cocktailTitle = resultCocktail.title;
+  cocktailImg = resultCocktail.image;
+
+  return resultCocktail;
+};
+*/
+
+
+// ------------------------------------
+//          Duplicate Check
+// ------------------------------------
+// Search function that returns true
+// or false if there is a duplicate in
+// the array searchHistory.
+// ------------------------------------
+function duplicateCheck(input) {
+  for (i = 0; i < searchBox.History.length; i++) {
+    if (input === searchHistory[i]) {
+      searchHistory.splice(i, 1)
+      searchHistory.unshift(input);
+    }
+    else {
+      return false;
+    }
+  }
+};
 // ------------------------------------
 //          Details Modal
 // ------------------------------------
@@ -337,16 +392,3 @@ function pairCocktail(cuisineData) {
 // pairing. Details will come from
 // pairCocktail().
 // ------------------------------------
-
-$("#searchHistory").empty();
-
-searchHistory.forEach(function (item) {
-  var historyBtn = $("<button>")
-    .addClass("search-history-btn")
-    .text(item)
-    .attr("data-search", item);
-  $("#searchHistory").append(historyBtn);
-});
-
-
-$(".longer.modal").modal("show");  
